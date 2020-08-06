a459999d231de59723805f91df7f1a263b61aa53
LpAnto <46076347+LpAnto@users.noreply.github.com>
2020-08-06 02:44:30 +0200

    ajout d'un compteur
    js

---
+++ js/coutdownwinter.js
@@ -0,0 +1,29 @@
+<p id="compteur"></p>
+
+<script>
+
+var DatecountDown = new Date("Dec 20, 2021 23:00:00").getTime();
+
+var x = setInterval(function() {
+
+  var DateNow = new Date().getTime();
+
+  var distance = DatecountDown - DateNow;
+
+  // Time calculations for days, hours, minutes and seconds
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  // Display the result in the element with id="demo"
+  document.getElementById("compteur").innerHTML = jours + "d " + heures + "h "
+  + minutes + "m " + secondes + "s ";
+
+  // If the count down is finished, write some text
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("compteur").innerHTML = "C'est l'hiver !!! olala il fait froid.";
+  }
+}, 1000);
+</script>
