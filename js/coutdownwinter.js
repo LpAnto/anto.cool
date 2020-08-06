0294157266a4a89f2c1c8ebade51e12709726b42
LpAnto <46076347+LpAnto@users.noreply.github.com>
2020-08-06 03:01:10 +0200

    shitty calculation errors
    lol

--- js/coutdownwinter.js
+++ js/coutdownwinter.js
@@ -7,14 +7,14 @@ var x = setInterval(function() {
   var distance = DatecountDown - DateNow;
 
   // Time calculations for days, hours, minutes and seconds
-  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+  var d = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var h = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var m = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var s = Math.floor((distance % (1000 * 60)) / 1000);
 
   // Display the result in the element with id="demo"
-  document.getElementById("compteur").innerHTML = jours + "d " + heures + "h "
-  + minutes + "m " + secondes + "s </h1>";
+  document.getElementById("compteur").innerHTML = d + "jours " + h + "heures "
+  + m + "minutes " + s + "secondes </h1>";
 
   // If the count down is finished, write some text
   if (distance < 0) {
