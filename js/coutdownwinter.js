338a13df60988869ab51ccbc818ae6c77dec1b0b
LpAnto <46076347+LpAnto@users.noreply.github.com>
2020-08-08 22:36:38 +0200

    Update coutdownwinter.js

--- js/coutdownwinter.js
+++ js/coutdownwinter.js
@@ -6,17 +6,14 @@ var x = setInterval(function() {
 
   var distance = DatecountDown - DateNow;
 
-  // Time calculations for days, hours, minutes and seconds
   var d = Math.floor(distance / (1000 * 60 * 60 * 24));
   var h = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
   var m = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
   var s = Math.floor((distance % (1000 * 60)) / 1000);
 
-  // Display the result in the element with id="demo"
   document.getElementById("compteur").innerHTML = d + " jours " + h + " heures "
   + m + " minutes " + s + " secondes </h1>";
 
-  // If the count down is finished, write some text
   if (distance < 0) {
     clearInterval(x);
     document.getElementById("compteur").innerHTML = "C'est l'hiver !!! olala il fait froid.";
